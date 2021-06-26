33d0280c6f5057103a43c82d9c83a110fd409552
ziw <ziw@users.noreply.github.com>
2021-06-26 09:27:41 -0700

    Support downloading overlayed video from s3 bucket (#20)

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,6 +5,7 @@ import './VideoList.css';
 import authenticatedUserState from '../recoils/authenticatedUserState';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import videoListState from '../recoils/videoListState';
+import Tooltip from '@material-ui/core/Tooltip';
 import { Storage } from 'aws-amplify';
 import { S3_SRT_BUCKET, S3_VIDEO_BUCKET } from '../constants/buckets';
 
@@ -16,13 +17,14 @@ function VideoList() {
 
   async function loadVideos() {
     console.log('listing videos ');
-    const [uploadedVideos, srtSubtitles /*transcribedVideos */] =
-      await Promise.all([
+    const [uploadedVideos, srtSubtitles, transcribedVideos] = await Promise.all(
+      [
         Storage.list(`${username}/`),
         Storage.list(`${username}/`, { bucket: S3_SRT_BUCKET }),
-        // TODO enable when transcribed video is ready
-        // Storage.list(`${username}/`, { bucket: S3_VIDEO_BUCKET }),
-      ]);
+        Storage.list(`${username}/`, { bucket: S3_VIDEO_BUCKET }),
+      ]
+    );
+
     uploadedVideos.forEach((video) => {
       const fileNameWithoutExtension = video.key.substr(
         0,
@@ -39,8 +41,26 @@ function VideoList() {
         video.fileNameWithoutExtension = fileNameWithoutExtension;
         video.fileName = video.key.substr(username.length + 1);
       }
+
+      if (
+        transcribedVideos.some(
+          (transcribedVideoFile) => transcribedVideoFile.key === video.key
+        )
+      ) {
+        video.transcribedVideoFileReady = true;
+        video.fileNameWithoutExtension = fileNameWithoutExtension;
+        video.fileName = video.key.substr(username.length + 1);
+      }
     });
     setVideoList(uploadedVideos);
+  }
+
+  async function downloadOverlayVideo(video) {
+    const signedURL = await Storage.get(video.key, {
+      bucket: S3_VIDEO_BUCKET,
+    });
+    setDownloadLink(signedURL);
+    downloadEle.current.click();
   }
 
   async function downloadSubtitle(video) {
@@ -61,12 +81,29 @@ function VideoList() {
           <Button
             variant="contained"
             color="primary"
+            style={{ margin: '10px auto' }}
             onClick={() => downloadSubtitle(video)}
           >
             Download Subtitle
           </Button>
         ) : (
-          <CircularProgress />
+          <Tooltip title="Processing your subtitle" aria-label="add">
+            <CircularProgress />
+          </Tooltip>
+        )}
+        {video.transcribedVideoFileReady ? (
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: '10px auto' }}
+            onClick={() => downloadOverlayVideo(video)}
+          >
+            Download Video
+          </Button>
+        ) : (
+          <Tooltip title="Processing your video" aria-label="add">
+            <CircularProgress />
+          </Tooltip>
         )}
         <span className="video-list__list-item-name">{video.fileName}</span>
       </div>
@@ -80,6 +117,8 @@ function VideoList() {
         ref={downloadEle}
         href={downloadLink}
         style={{ display: 'none' }}
+        target="_blank"
+        rel="noreferrer"
       >
         download
       </a>
